164d7e5c29d7dbae456647f79aa1a1891b6f0395
Rodrigo Reyes <rodrigo.m.reyes.v@gmail.com>
2025-03-31 10:46:18 -0600

    add pins

---
+++ SimonDice/pins.h
@@ -0,0 +1,14 @@
+#define PINS_H
+#ifdef PINS_H
+
+#define PLAYER1_BLUE    17
+#define PLAYER1_RED    27
+#define PLAYER1_YELLOW 22
+#define PLAYER1_GREEN   5
+
+#define PLAYER2_BLUE    23
+#define PLAYER2_RED    24
+#define PLAYER2_YELLOW 25
+#define PLAYER2_GREEN   16
+
+#endif
