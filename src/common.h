4d1b4ef18452917f8b82bd862484fd7ff5b3c74c
Rodrigo Reyes <rodrigo.m.reyes.v@gmail.com>
2025-05-18 13:41:18 -0600

    definitions

--- src/common.h
+++ src/common.h
@@ -12,11 +12,18 @@ typedef enum {
     RED
 } COLORNUMBERS;
 
-static const std::map<int, COLORNUMBERS> color_number_map = {
+static const std::map<int, int> color_input_map = {
     {INPUT_VALUE_YELLOW, YELLOW},
     {INPUT_VALUE_GREEN, GREEN},
     {INPUT_VALUE_BLUE, BLUE},
     {INPUT_VALUE_RED, RED}
+};
+
+static const std::map<int, int> color_output_map = {
+    {YELLOW, OUTPUT_VALUE_YELLOW},
+    {GREEN, OUTPUT_VALUE_GREEN},
+    {BLUE, OUTPUT_VALUE_BLUE},
+    {RED, OUTPUT_VALUE_RED}
 };
 
 static const std::map<int, std::string> color_name_map = {
