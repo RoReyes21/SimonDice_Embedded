85ed23ad159e51d2eb17b2bf0ea7131c6a59f2f4
Rodrigo Reyes <rodrigo.m.reyes.v@gmail.com>
2025-05-17 21:07:37 -0600

    add inputs value for push bottons

--- src/pins.h
+++ src/pins.h
@@ -1,14 +1,9 @@
 #define PINS_H
 #ifdef PINS_H
 
-#define PLAYER1_BLUE    17
-#define PLAYER1_RED    27
-#define PLAYER1_YELLOW 22
-#define PLAYER1_GREEN   5
-
-#define PLAYER2_BLUE    23
-#define PLAYER2_RED    24
-#define PLAYER2_YELLOW 25
-#define PLAYER2_GREEN   16
+#define INPUT_VALUE_YELLOW  16
+#define INPUT_VALUE_GREEN   32
+#define INPUT_VALUE_BLUE    64
+#define INPUT_VALUE_RED     128
 
 #endif
